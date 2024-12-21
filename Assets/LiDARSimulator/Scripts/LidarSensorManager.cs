b2e675e8a97d573172bdb799abe119ced6b58e10
Lilacsky <lilacsky824@gmail.com>
2024-12-21 08:18:12 +0800

    update: change lidar sensor's reference type

--- Assets/LiDARSimulator/Scripts/LidarSensorManager.cs
+++ Assets/LiDARSimulator/Scripts/LidarSensorManager.cs
@@ -1,126 +1,143 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Pool;
 using AWSIM;
-using UnityEngine.Serialization;
+using RGLUnityPlugin;
 
-[Serializable]
-public class LidarSensorManager : MonoBehaviour
+namespace LiDARSimulator
 {
-    [SerializeField] private RglLidarPublisher _sensorPrefab;
-    [SerializeField, Range(0, 1)] private float _activeSensorRatio = 0.2f;
-    [SerializeField] private int _maxSensorLimit = 10;
-    [SerializeField] private Transform[] _targetTransforms;
-    
-    [SerializeField] private string _publisherTopicName = "lidar/pointcloud_sim_";
-
-    private ObjectPool<RglLidarPublisher> _sensorPool;
-    private HashSet<RglLidarPublisher> _activeSensors = new HashSet<RglLidarPublisher>();
-    private Queue<Transform> _candidateTransforms = new Queue<Transform>();
-
-    public int ActiveSensorCount
+    /// <summary>
+    /// Manage multiple lidar sensors with object pool, can change amount dynamically.
+    /// </summary>
+    public class LidarSensorManager : MonoBehaviour
     {
-        get => _activeSensors.Count;
-        set { AdjustSensorCount(value, out _); }
-    }
+        [SerializeField] private LidarSensor _sensorPrefab;
+        [SerializeField, Range(0, 1)] private float _activeSensorRatio = 0.2f;
+        [SerializeField] private int _maxSensorLimit = 10;
+        
+        ///TODO: Not sure how to determine the position of the LiDAR sensor, so for now, it will be placed manually.
+        [SerializeField] private Transform[] _targetTransforms;
+        ///TODO: How to sync topic name to ROS2 packages? Via sending ROS2 messages?
+        [SerializeField] private string _pointCloudTopicName = "lidar/pointcloud_sim_";
 
-    public RglLidarPublisher GetSensor()
-    {
-        AdjustSensorCount(ActiveSensorCount + 1, out RglLidarPublisher sensor);
-        return sensor;
-    }
+        private ObjectPool<LidarSensor> _sensorPool;
+        private readonly HashSet<LidarSensor> _activeSensors = new();
+        private Queue<Transform> _candidateTransforms = new();
 
-    public void ReturnSensor(RglLidarPublisher sensor)
-    {
-        OnReleaseToPool(sensor);
-    }
-
-    private void Awake()
-    {
-        _sensorPool = new ObjectPool<RglLidarPublisher>(
-            createFunc: CreateSensor,
-            actionOnGet: OnGetFromPool,
-            actionOnRelease: OnReleaseToPool,
-            actionOnDestroy: OnDestroyPoolObject,
-            maxSize: _maxSensorLimit
-        );
-
-        _candidateTransforms = TransformUtility.FindTransformsByName(_targetTransforms.name).ToQueue();
-
-        ActiveSensorCount = _maxSensorLimit;
-    }
-
-    private RglLidarPublisher CreateSensor()
-    {
-        RglLidarPublisher newSensor = Instantiate(_sensorPrefab);
-        newSensor.gameObject.SetActive(false);
-        return newSensor;
-    }
-
-    private void OnGetFromPool(RglLidarPublisher sensor)
-    {
-        sensor.gameObject.SetActive(true);
-        _activeSensors.Add(sensor);
-
-        sensor.pointCloud2Publishers[0].topic = _publisherTopicName + (uint)sensor.GetInstanceID();
-    }
-
-    private void OnReleaseToPool(RglLidarPublisher sensor)
-    {
-        sensor.gameObject.SetActive(false);
-        _activeSensors.Remove(sensor);
-        _candidateTransforms.Enqueue(sensor.transform.parent);
-    }
-
-    private void OnDestroyPoolObject(RglLidarPublisher sensor)
-    {
-        Destroy(sensor.gameObject);
-    }
-
-    public void SetSensorParent(RglLidarPublisher sensor, Transform parentTransform)
-    {
-        sensor.transform.SetParent(parentTransform);
-        sensor.transform.localPosition = Vector3.zero;
-    }
-
-    private void AdjustSensorCount(int targetCount, out RglLidarPublisher sensor)
-    {
-        targetCount = Mathf.Clamp(targetCount, 0, _maxSensorLimit);
-
-        while (_activeSensors.Count < targetCount)
+        public int ActiveSensorCount
         {
-            sensor = _sensorPool.Get();
-            SetSensorParent(sensor, _candidateTransforms.Dequeue());
+            get => _activeSensors.Count;
+            set => AdjustSensorCount(value, out _);
         }
 
-        while (_activeSensors.Count > targetCount)
+        public float ActiveSensorRatio
         {
-            RglLidarPublisher sensorToRelease = _activeSensors.Single();
-            _sensorPool.Release(sensorToRelease);
-            _activeSensors.Remove(sensorToRelease);
+            get => _activeSensorRatio;
+            set
+            {
+                _activeSensorRatio = value;
+                ActiveSensorCount = Mathf.RoundToInt(_maxSensorLimit * ActiveSensorRatio);
+            }
+        }
+
+        public LidarSensor GetSensor()
+        {
+            AdjustSensorCount(ActiveSensorCount + 1, out LidarSensor sensor);
+            return sensor;
+        }
+
+        public void ReturnSensor(LidarSensor sensor)
+        {
+            OnReleaseToPool(sensor);
+        }
+
+        private void Awake()
+        {
+            _sensorPool = new ObjectPool<LidarSensor>(
+                createFunc: CreateSensor,
+                actionOnGet: OnGetFromPool,
+                actionOnRelease: OnReleaseToPool,
+                actionOnDestroy: OnDestroyPoolObject,
+                maxSize: _maxSensorLimit
+            );
+
+            _candidateTransforms = new Queue<Transform>(_targetTransforms);
+
+            ActiveSensorRatio = _activeSensorRatio;
+        }
+
+        private LidarSensor CreateSensor()
+        {
+            LidarSensor newSensor = Instantiate(_sensorPrefab);
+            RglLidarPublisher publisher = newSensor.GetComponent<RglLidarPublisher>();
+            // Assign a unique id to prevent topic id conflict.
+            publisher.pointCloud2Publishers[0].topic = _pointCloudTopicName + (uint)newSensor.GetInstanceID();
+            newSensor.gameObject.SetActive(false);
+            return newSensor;
+        }
+
+        private void OnGetFromPool(LidarSensor sensor)
+        {
+            sensor.gameObject.SetActive(true);
+            _activeSensors.Add(sensor);
+        }
+
+        private void OnReleaseToPool(LidarSensor sensor)
+        {
+            sensor.gameObject.SetActive(false);
+            _activeSensors.Remove(sensor);
+            _candidateTransforms.Enqueue(sensor.transform.parent);
+        }
+
+        private void OnDestroyPoolObject(LidarSensor sensor)
+        {
+            Destroy(sensor.gameObject);
+        }
+
+        public void PlaceLidarSensorToTransform(LidarSensor sensor, Transform parentTransform)
+        {
+            sensor.transform.SetParent(parentTransform);
+            sensor.transform.localPosition = Vector3.zero;
+        }
+
+        private void AdjustSensorCount(int targetCount, out LidarSensor sensor)
+        {
+            targetCount = Mathf.Clamp(targetCount, 0, _maxSensorLimit);
+
+            while (_activeSensors.Count < targetCount)
+            {
+                sensor = _sensorPool.Get();
+                PlaceLidarSensorToTransform(sensor, _candidateTransforms.Dequeue());
+            }
+
+            while (_activeSensors.Count > targetCount)
+            {
+                LidarSensor sensorToRelease = _activeSensors.First();
+                _sensorPool.Release(sensorToRelease);
+                _activeSensors.Remove(sensorToRelease);
+                sensor = null;
+            }
+
             sensor = null;
         }
 
-        sensor = null;
-    }
-
-    public void ReleaseAllSensors()
-    {
-        var sensorsToRelease = new List<RglLidarPublisher>(_activeSensors);
-        foreach (var sensor in sensorsToRelease)
+        public void ReleaseAllSensors()
         {
-            _sensorPool.Release(sensor);
+            var sensorsToRelease = new List<LidarSensor>(_activeSensors);
+            foreach (var sensor in sensorsToRelease)
+            {
+                _sensorPool.Release(sensor);
+            }
+
+            _activeSensors.Clear();
         }
 
-        _activeSensors.Clear();
-    }
-
-    private void OnDestroy()
-    {
-        _sensorPool.Dispose();
-        _activeSensors.Clear();
-        _candidateTransforms.Clear();
+        private void OnDestroy()
+        {
+            _sensorPool.Dispose();
+            _activeSensors.Clear();
+            _candidateTransforms.Clear();
+        }
     }
 }
